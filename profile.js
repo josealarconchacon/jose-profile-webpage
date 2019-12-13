29f0c36540e0d04cb2b36a56697bfb50d15a9456
josealarconchacon <jose.alarcon.chacom@gmail.com>
2019-12-13 15:33:41 -0500

    add animation to navbar

--- profile.js
+++ profile.js
@@ -1,19 +1,20 @@
 
-$(document).ready(function() {
-            $(".menu-icon").on("click", function() {
-                  $("nav ul").toggleClass("showing");
-            });
-      });
+// $(document).ready(function() {
+//             $(".menu-icon").on("click", function() {
+//                   $("nav ul").toggleClass("showing");
+//             });
+//       });
 
-      // Scrolling Effect
+//       // Scrolling Effect
 
-      $(window).on("scroll", function() {
-            if($(window).scrollTop()) {
-                  $('nav').addClass('black');
-            }
+//       $(window).on("scroll", function() {
+//             if($(window).scrollTop()) {
+//                   $('nav').addClass('black');
+//             }
 
-            else {
-                  $('nav').removeClass('black');
-            }
-      })
+//             else {
+//                   $('nav').removeClass('black');
+//             }
+//       })
+      
       
